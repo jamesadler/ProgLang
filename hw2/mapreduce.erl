26bdbc34d10c616d8291bcb7022e253af2bbdac1
jamesadler <jfa@jamesfadler.com>
2015-10-29 01:11:32 -0400

    goodie
    fuck this shit

--- hw2/mapreduce.erl
+++ hw2/mapreduce.erl
@@ -1,6 +1,11 @@
 -module(mapreduce) .
 -export( [start/8] ).
 
+% start("input_file.txt","output_file.txt",fun dna_char_count:map/1,
+% 		fun dna_char_count:reduce/1,4,2,2,
+% 		['workplace1@127.0.0.1','workplace2@127.0.0.1','workplace3@127.0.0.1'])
+% $ erl -noshell name workplace1@127.0.0.1 -setcookie mapreduce
+
 start (Fin, Fout, Mapf, Redf, Num_m, Num_s, Num_r, Nodes) ->
   io:format("Cool beans!~n"),
   make_host_file (Nodes)
