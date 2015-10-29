8c6dc66409803a847026fb3d105f65c1ef430f59
Spencer Norris <spencercnorris2017@gmail.com>
2015-10-28 21:51:17 -0400

    we're so screwed

---
+++ hw2/mapreduce.erl
@@ -0,0 +1,24 @@
+-module(mapreduce) .
+-export( [start/8] ).
+
+start (Fin, Fout, Mapf, Redf, Num_m, Num_s, Num_r, Nodes) ->
+  io:format("Cool beans!~n"),
+  make_host_file (Nodes)
+  %map_phase(mapf, fin, nodes)
+  .
+
+
+%map_phase(Mapf, Fin, Nodes) ->
+
+%.
+
+
+%spawn_nodes(Mapf, [H|T]) ->
+
+%.
+
+
+%% Generate host file from Node names
+make_host_file(Nodes) ->
+  file:open( ".hosts.erlang", [read, write]),
+  lists:foreach( fun(X) -> file:write(".hosts.erlang", "\'~p\'~n.", [X]) end, Nodes ).
