80fe7a836901d7327adfe5c3f1ba844ae39d9ab9
Spencer Norris <spencercnorris2017@gmail.com>
2015-11-01 06:40:50 -0500

    Major changes to mapreduce.erl; renamed make_host_file to spawn_mappers, accepts different set of parameters. Vyvanse is one hell of a drug

--- hw2/mapreduce.erl
+++ hw2/mapreduce.erl
@@ -15,30 +15,40 @@ loop(Max, Func) ->
 
 % Input File, Output File, Map func, Reduce func, Num of Mappers,
 % Num of Shufflers, Num of Reducers, List of Node names
-start(Fin, Fout, Mapf, Redf, Num_m, Num_s, Num_r, Nodes) ->
+start(Fin, Fout, FMod, Mapf, Redf, Num_m, Num_s, Num_r, Nodes) ->
 
-	make_host_file(Nodes),
+
+	%Read in input file line by line, spawn mapper for each
 	{ok, Device} = file:open(Fin,[read]),
+	make_host_file(Device, Nodes, 0, FMod, Mapf),
 
 	% Lines = [],
 	Lines = read_line(Device, []),
 	io:format("~w",[Lines]),
+
+
+	% Mapping Phase
+	%lists:map( fun(X) -> spawn(X, ) end, Nodes),
+
+
+
+
 	% register(mapreduceAtom, spawn(mapreduce, map_phase, [Device, Mapf, Redf])),
 
 	% Account = 0,
 	% ReducePhase = loop(Num_r,
 	% 	fun(_) -> spawn(mapreduce, reduce_phase, [Redf, Account]) end
 	% ),
-  	
+
   	io:format("End of Cool Beans!~n")
 .
 
 read_line(Device, Lines) ->
 	case io:get_line(Device, "") of
-		eof -> 
-			file:close(Device), 
+		eof ->
+			file:close(Device),
 			io:format("End of File\n");
-		Line -> 
+		Line ->
 			Tmp = string:tokens(Line, "\t"),
 			Key = lists:nth(1,Tmp),
 			Value = string:strip(lists:nth(2,Tmp), right, $\n),
@@ -47,7 +57,7 @@ read_line(Device, Lines) ->
 			% io:format("~w\n",[Value])
 			% [Value],
 			% TmpMap = mapper:map({Key,Value}),
-			
+
 			% shuffle_phase(TmpMap, MappedData)
 			% % map_phase(Device, Mapf, Redf)
 			read_line(Device, [[Value] | Lines])
@@ -59,24 +69,24 @@ reduce_phase(Redf, Account) ->
 .
 
 map_phase(Device, Redf) ->
-	
+
 	io:format("func: mapreduce\n"),
 
 	MappedData = [],%[ {67,[]} ],%[{67,[]},{71,[]},{84,[]}],
 	case io:get_line(Device, "") of
 		eof -> file:close(Device), io:format("End of File\n");
-		Line -> 
+		Line ->
 			Tmp = string:tokens(Line, "\t"),
 			Key = lists:nth(1,Tmp),
 			Value = string:strip(lists:nth(2,Tmp), right, $\n),
 
 			TmpMap = mapper:map({Key,Value}),
-		
+
 			shuffle_phase(TmpMap, MappedData)
 	end,
 
 
-	io:format("fund: end mapreduce\n")
+	io:format("fund: end map phase \n")
 .
 
 shuffle_phase(Data, MappedData) ->
@@ -109,17 +119,27 @@ shuffle_phase(Data, MappedData) ->
 .
 
 
-% Generate host file from Node names
-make_host_file(Nodes) ->
-  	{ok, Device} = file:open(".hosts.erlang", [write]),
+% Generate mappers from input file, distributing among nodes
+spawn_mappers(Device, Nodes, Index, FMod, Mapf) ->
 
-  	lists:foreach(
-  		fun(X) -> 
-  			% Formats the host name to net_adm standards
-  			file:write(Device, io_lib:format("\'~s\'.\n", [X])) 
-  		end,
-  		Nodes
-  	),
+%{ok, Device} = file:open(".hosts.erlang", [write]),
+% Formats the host name to net_adm standards
+%file:write(Device, io_lib:format("\'~s\'.\n", [X])),
 
-  	file:close(Device)
-.
+
+case io:get_line(Device, "") of
+	eof -> file:close(Device), io:format("End of File\n");
+	Line ->
+		Tmp = string:tokens(Line, "\t"),
+		Key = lists:nth(1,Tmp),
+		Value = string:strip(lists:nth(2,Tmp), right, $\n),
+		Pid = spawn(X, FMod, Mapf, []),
+		Pid ! { self(), {Key, Value} },
+
+		%Cycle through Nodes to distribute workload
+		if(Length(Nodes) =:= Index)
+			spawn_mappers(Device, Nodes, 0, FMod, Mapf),
+		else
+			spawn_mappers(Device, Nodes, Index + 1, FMod, Mapf)
+
+end.
---
+++ hw2/test_print.erl
@@ -0,0 +1,6 @@
+-module(test_print) .
+-export([spoon_print/0]) .
+
+spoon_print() ->
+  io:format("Cool beans!~n")
+.
