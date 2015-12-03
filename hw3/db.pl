f96deeccf9f5470d7fdcc2e29a58d400ba4612b1
jamesadler <jfa@jamesfadler.com>
2015-12-03 16:12:39 -0500

    loop
    added the loop function

--- hw3/db.pl
+++ hw3/db.pl
@@ -1,5 +1,3 @@
-
-
 det(the).
 det(a).
 det(every).
@@ -22,37 +20,72 @@ pres_verb(fly).
 pres_verb(arrive).
 pres_verb(stay).
 
-
-np(X, Y):- X = det(D), Y = noun(N), det(D), noun(N).
+np(X, Y):- X = det(D), Y = nom(noun(N)), det(D), nom(noun(N)).
 vp(X):- X = verb(V), verb(V).
 nom(X):- X = noun(N), noun(N).
 s(X, Y):- X = np(D, N), Y = vp(V), np(D, N), vp(V).
 
-cmd(X):- X = det(D), D.
+%VERB MAPPINGS
+maps(verb(left), pres_verb(leave)).
+maps(verb(arrived), pres_verb(arrive)).
+maps(verb(stayed), pres_verb(stay)).
+maps(verb(flew), pres_verb(fly)).
 
-print_list([]).
-print_list([H|T]):- nl, write(H), print_list(T).
+%QUANTIFIERS
+quantifier(a).
+quantifier(every).
 
-parse([W1, W2, W3, W4, '.']):-
-  A = det(W1), A,
-  B = nom(noun(W2)), B,
-  assert(word(W3)),
-  D = verb(W4), D,
-  listing(word)
-.
+%ASSERTION PREDICATE
+%assertion(nom(noun(N)), atom(Name), verb(V)).
 
-%% parse([W1, W2, W3, W4, W5, W6, '.']):-
-%% X = det(W1), X,
-%% Y = nom(noun(W2)), Y,
-%% Z = verb(W3), Z,
-%% NP = np(X, Y), VP = vp(Z), writeln(s(NP, VP))
-%% writeln(X)
-%% .
+%CONTRAPOSITIVE ASSERTIONS
+parse(['the', W1, Name, 'did', 'not', W2, '.']):-
+  A = nom(noun(W1)), A,
+  B = pres_verb(W2), B,
+  assert(assertion(A, Name, not(B))), !.
+
+%POSITIVE ASSERTIONS
+parse(['the', W1, Name, W2, '.']):-
+  A = nom(noun(W1)), A,
+  B = verb(W2), B,
+  assert(assertion(A, Name, B)), !.
+
+%QUERIES
+parse(['did', W2, W3, W4, '?']):-
+  quantifier(W2),
+  nom(noun(W3)),
+  pres_verb(W4),
+  (query(W2, W3, W4)->
+    write(yes);
+    write(no)
+  ).
+
+query('a', N, PV):-
+  maps(V, pres_verb(PV)),
+  predicate_property(assertion(nom(noun(N)), _, V), visible), !.
+
+query('every', N, PV):-
+  maps(V, pres_verb(PV)),
+
+  /*cannot be predicate saying person did not 'verb',*/
+  not(predicate_property(assertion(nom(noun(N)), _, not(V)), visible)),
+
+  /*Also cannot be the case...*/
+  not(
+      (
+        /*that for a list L containing all the names of all nouns of type N...*/
+        findall(X, predicate_property(assertion(nom(noun(N)), X, _), visible), L),
+
+              /*there exists an element with name Var that has not done verb V*/
+              member(Var, L),
+
+              not(predicate_property(assertion(nom(noun(N)), Var, V), visible))
+      )
+  ), !.
 
 loop :-
   read_line(L),
   (L \= ['.'], L \= ['!']),
-  %% writeln(L),
   /*print_list(L),*/
   parse(L),
   loop.
@@ -102,3 +135,4 @@ space(32).
 period(46).
 question_mark(63).
 */
+
