1565982359c844f908329f57cb0daa998e821b1d
jamesadler <jfa@jamesfadler.com>
2015-12-03 03:57:39 -0500

    Database
    init db.pl

---
+++ hw3/db.pl
@@ -0,0 +1,104 @@
+
+
+det(the).
+det(a).
+det(every).
+
+det(did).
+det(not).
+
+noun(train).
+noun(bike).
+noun(flight).
+noun(person).
+
+verb(flew).
+verb(left).
+verb(arrived).
+verb(stayed).
+
+pres_verb(leave).
+pres_verb(fly).
+pres_verb(arrive).
+pres_verb(stay).
+
+
+np(X, Y):- X = det(D), Y = noun(N), det(D), noun(N).
+vp(X):- X = verb(V), verb(V).
+nom(X):- X = noun(N), noun(N).
+s(X, Y):- X = np(D, N), Y = vp(V), np(D, N), vp(V).
+
+cmd(X):- X = det(D), D.
+
+print_list([]).
+print_list([H|T]):- nl, write(H), print_list(T).
+
+parse([W1, W2, W3, W4, '.']):-
+  A = det(W1), A,
+  B = nom(noun(W2)), B,
+  assert(word(W3)),
+  D = verb(W4), D,
+  listing(word)
+.
+
+%% parse([W1, W2, W3, W4, W5, W6, '.']):-
+%% X = det(W1), X,
+%% Y = nom(noun(W2)), Y,
+%% Z = verb(W3), Z,
+%% NP = np(X, Y), VP = vp(Z), writeln(s(NP, VP))
+%% writeln(X)
+%% .
+
+loop :-
+  read_line(L),
+  (L \= ['.'], L \= ['!']),
+  %% writeln(L),
+  /*print_list(L),*/
+  parse(L),
+  loop.
+
+
+read_line(Words) :- get0(C),
+                    read_rest(C,Words).
+
+/* A period or question mark ends the input. */
+read_rest(46,['.']) :- !.
+read_rest(63,['?']) :- !.
+
+/* Spaces and newlines between words are ignored. */
+read_rest(C,Words) :- ( C=32 ; C=10 ) , !,
+                     get0(C1),
+                     read_rest(C1,Words).
+
+/* Commas between w;ords are absorbed. */
+read_rest(44,[','|Words]) :- !,
+                             get0(C1),
+                             read_rest(C1,Words).
+
+/* Otherwise get all of the next word. */
+read_rest(C,[Word|Words]) :- lower_case(C,LC),
+                             read_word(LC,Chars,Next),
+                             name(Word,Chars),
+                             read_rest(Next,Words).
+
+/* Space, comma, newline, period or question mark separate words. */
+read_word(C,[],C) :- ( C=32 ; C=44 ; C=10 ;
+                         C=46 ; C=63 ) , !.
+
+/* Otherwise, get characters, convert alpha to lower case. */
+read_word(C,[LC|Chars],Last) :- lower_case(C,LC),
+                                get0(Next),
+                                read_word(Next,Chars,Last).
+
+/* Convert to lower case if necessary. */
+lower_case(C,C) :- ( C <  65 ; C > 90 ) , !.
+lower_case(C,LC) :- LC is C + 32.
+
+
+/* for reference ...
+newline(10).
+comma(44).
+space(32).
+period(46).
+question_mark(63).
+*/
